ef0ab68ed73ff3e5454667513cedd6b2889f18e7
HemishPansuriya <hemishnandlal@gmail.com>
2026-01-27 22:19:59 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -21,7 +21,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",   # Vite frontend
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://trading-signal-saas.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
